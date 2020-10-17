e7675650a34db26b4586229afd4689956eca43de
Dmytro Vasylenko <tredstoun651@uk.net>
2020-10-17 18:56:00 +0300

    добавляет изменения

--- gallery.js
+++ gallery.js
@@ -5,8 +5,9 @@ const buttonEl = document.querySelector('[data-action="close-lightbox"]');
 const overlayEl = document.querySelector(".lightbox__overlay");
 const lightboxEl = document.querySelector(".lightbox");
 const lightboxImageEl = document.querySelector(".lightbox__image");
-
-
+const sliderControllLeft = document.querySelector('.slider__control_left');
+const sliderControllRight = document.querySelector('.slider__control_right');
+let index = 0;
 
 //Создание и рендер разметки по массиву данных и предоставленному шаблону.
 
@@ -47,8 +48,8 @@ galleryEl.append(...photoCard);
 //Реализация делегирования на галерее ul.js-gallery и получение url большого изображения.
 
 galleryEl.addEventListener("click", onImageClick);
-buttonEl.addEventListener("click", onButtonClick);
-overlayEl.addEventListener("click", onOverlayClick);
+buttonEl.addEventListener("click", onButtonCloseModalClick);
+
 
 function createElement(name, attrs = {}) {
   const element = document.createElement(name);
@@ -59,23 +60,26 @@ function createElement(name, attrs = {}) {
 }
 
 function onImageClick(event) {
+
   if (event.target.nodeName !== "IMG") {
     return;
   }
-  let currentIndex = event.target.dataset.index;
-  const cardEl = event.target;
-  const imageUrl = cardEl.dataset.source;
-  const imageAlt = cardEl.getAttribute('alt');
+
+  const imageUrl = event.target.dataset.source;
+  const imageAlt = event.target.getAttribute('alt');
 
   // const parentCard = cardEl.closest(".gallery__item");
   openModal();
   changesValueAttribute(imageUrl, imageAlt);
-  
+  index = +event.target.dataset.index;
 }
 
 //Открытие модального окна по клику на элементе галереи.
 function openModal() {
-  window.addEventListener("keydown", onEscKeyDown);
+  window.addEventListener("keydown", onKeyDown);
+  overlayEl.addEventListener("click", onOverlayClick);
+  sliderControllLeft.addEventListener('click', prev);
+  sliderControllRight.addEventListener('click', next);
   lightboxEl.classList.add("is-open");
 }
 
@@ -85,7 +89,7 @@ function changesValueAttribute(url, alt) {
   lightboxImageEl.setAttribute("alt", `${alt}`);
 }
 
-function onButtonClick(event) {
+function onButtonCloseModalClick(event) {
   if (event.target.nodeName !== "BUTTON") {
     return;
   }
@@ -95,7 +99,10 @@ function onButtonClick(event) {
 
 //Закрытие модального окна по клику на кнопку button[data-action="close-lightbox"]
 function closeModal() {
-  window.removeEventListener("keydown", onEscKeyDown);
+  window.removeEventListener("keydown", onKeyDown);
+  overlayEl.removeEventListener("click", onOverlayClick);
+  sliderControllLeft.removeEventListener('click', prev);;
+   sliderControllRight.removeEventListener('click', next);
   lightboxEl.classList.remove("is-open");
 }
 
@@ -112,44 +119,61 @@ function onOverlayClick(event) {
   }
 }
 
-function onEscKeyDown(event) {
+function onKeyDown(event) {
   if (event.code === "Escape") {
     closeModal();
     attributeClearing();
   }
+  if (event.code === 'ArrowRight') next();
+  if (event.code === 'ArrowLeft') prev();
+}
+
+function next() {
+  
+  const nextIndex = ++index;
+  photos[nextIndex]
+    ? lightboxImageEl.src = photos[nextIndex].original
+    : index = 0, lightboxImageEl.src = photos[index].original
+ 
+}
+function prev() {
+  
+  const prevIndex = --index;
+  photos[prevIndex]
+    ? lightboxImageEl.src = photos[prevIndex].original
+    : index = photos.length - 1, lightboxImageEl.src = photos[index].original
 }
  
 //cлайдер
-const sliderControllLeft = document.querySelector('.slider__control_left');
-const sliderControllRight = document.querySelector('.slider__control_right');
-sliderControllLeft.addEventListener('click', previousImage)
-sliderControllRight.addEventListener('click', nextImage)
 
-function previousImage(event) {
 
-  let index = photos.findIndex((el) => {
-    return el.original === lightboxImageEl.getAttribute("src");
-  });
-if (index <= photos.length && index >= 0) {
-    index -= 1;
-  } else { return; }
-  const previousPic = photos[index].original;
-  lightboxImageEl.setAttribute("src", previousPic);
-}
 
-function nextImage(event) {
+// function previousImage(event) {
+// prev()
+//   let index = photos.findIndex((el) => {
+//     return el.original === lightboxImageEl.getAttribute("src");
+//   });
+// if (index <= photos.length && index >= 0) {
+//     index -= 1;
+//   } else { return; }
+//   const previousPic = photos[index].original;
+//   lightboxImageEl.setAttribute("src", previousPic);
+// }
+
+// function nextImage(event) {
+// next()
+  // let index = photos.findIndex((el) => {
+  //   return el.original === lightboxImageEl.getAttribute("src");
+  // });
   
-  let index = photos.findIndex((el) => {
-    return el.original === lightboxImageEl.getAttribute("src");
-  });
+  // if (index <= photos.length && index >= 0) {
+  //   index += 1;
+  // } else { return; }
   
-  if (index <= photos.length && index >= 0) {
-    index += 1;
-  } else { return; }
-  
-  const previousPic = photos[index].original;
-  lightboxImageEl.setAttribute("src", previousPic);
+  // const previousPic = photos[index].original;
+  // lightboxImageEl.setAttribute("src", previousPic);
 
-}
+// }
+
 
 
